7bc6729b2d2ab5feb79f7c9edb54ba8e344ae340
aabuzaid2 <a.abuzaid@aucegypt.edu>
2022-12-01 19:17:39 +0200

    co-author @adhammeligy co-author @salmaabdellatif

--- src/main.rs
+++ src/main.rs
@@ -66,15 +66,22 @@ fn handle_request(socket : &UdpSocket)
         println!("client sent : {}",client_reply);
         let reply = String::from("Ack");
         let reply =reply.as_bytes();
-        socket.send_to(reply, src_addr).expect("couldn't send data");
+        let reply_address = src_addr.to_string();
+        // let location = (reply_address.len()-1) as i32;
+        // reply_address[location] = "4";
+        let use_addr = reply_address.replace("7880", "7885");
+        println!("{}", use_addr);
+        socket.send_to(reply, &use_addr).expect("couldn't send data");
         // println!("Sent ack");
     }
 }
 //we can add token here as well to make it really sleep
 
 fn election(socket : &UdpSocket,gahez : &Arc<Mutex<i32>>) {
-    let my_local_ip = local_ip().unwrap();
-    let agent_list = [my_local_ip.to_string()+":7882", "10.40.55.44:7882".to_string()]; //my_local_ip.to_string()+":7882", 
+    // let my_local_ip = local_ip().unwrap();
+    let agent_list = ["192.168.8.106:7882", "192.168.8.107:7882"]; //my_local_ip.to_string()+":7882", 
+    let server_list = ["192.168.8.123:6000", "192.168.8.121:6000"]; //, "10.40..:6000" 
+    //let numservers = server_list.len()+1;
     loop 
     {
         let gahez_1 = {
@@ -93,37 +100,51 @@ fn election(socket : &UdpSocket,gahez : &Arc<Mutex<i32>>) {
             let str_rng = format!("{}",rng);
             println!("Sending {}", str_rng);
             let str_rng = str_rng.as_bytes();
+            //let mut rng_list = [i32;numservers+1];
+            let mut rng_list = [0,0,0];
+            rng_list[0] = rng;
+            for i in 0..server_list.len()
+            {
+                socket.send_to(str_rng, &server_list[i]).expect("couldn't send data");
+                println!("sent number to server {}", server_list[i]);
+                let mut error_flag = true;
+                while error_flag {
+                    let case = socket.recv_from(&mut buf);
+                    match case {
+                        Ok((_,src_addr)) => {
+                            println!("Recieved successsfully from {}",src_addr);
+                            error_flag = false;
+                        }
+                        Err(_) =>error_flag = true
+                    }
+                }
+
+                let first_digit = (buf[0] - 48)*10;
+                let second_digit = buf[1]-48;
+                let num = first_digit + second_digit;
+                // let num = 50;
+                rng_list[i+1] = num.into();
+                println!("i recieved number : {}",num);
+            }
             
 
-            socket.send_to(str_rng, "10.40.55.44:6000").expect("couldn't send data");
-            println!("sent number to other server");
-
-            let mut error_flag = true;
-            while error_flag {
-                let case = socket.recv_from(&mut buf);
-            match case {
-                Ok((_,src_addr)) => {
-                    println!("Recieved successsfully from {}",src_addr);
-                    error_flag = false;
+        
+            let mut max = 0;
+            for i in 0..rng_list.len()
+            {
+                if rng_list[i] > max{
+                    max = rng_list[i];
                 }
-                Err(_) =>error_flag = true
             }
-        }
-
-            let first_digit = (buf[0] - 48)*10;
-            println!("Second dig {}", buf[1]);
-            let second_digit = buf[1]-48;
-            let num = first_digit + second_digit;
-            // let num = 50;
-            println!("i recieved number : {}",num);
+            
 
             
         
             println!(" recieved number from other server");
             
-            if rng < num.into()
+            if max != rng_list[0]
             {
-                println!("I will not sleep bitches");
+                println!("I will not sleep ###############################");
             }
             else {
                 println!("Goodnight ###################################");
@@ -149,7 +170,9 @@ fn election(socket : &UdpSocket,gahez : &Arc<Mutex<i32>>) {
 
 fn gahzeen(socket : &UdpSocket, gahez : &Arc<Mutex<i32>>){
     loop{
+        let timer = time::Duration::from_secs(15);
         let mut bufg = [0;1000];
+        let server_list = ["192.168.8.121:2155", "192.168.8.123:2155"];
         let mut gahez_1 = {
                     
                     let mut gahez_1 = gahez.lock().unwrap();
@@ -157,40 +180,50 @@ fn gahzeen(socket : &UdpSocket, gahez : &Arc<Mutex<i32>>){
                     0
                     
             };
-                    
+            let mut gahez_list = [0,0,0];
+
+                
             while gahez_1 != 1 
             {
-                println!("ana mestanni fel gahez");
+                println!("ana mestanni fel gahez /////////////////////////////////////////");
                 let ana_gahez:i32 = 1;
-
+                gahez_list[0] = 1;
                 let eb3at_gahez = format!("{}",ana_gahez);
                 
                 let eb3at_gahez = eb3at_gahez.as_bytes();
+                for i in 0..server_list.len()
+                {
+                    socket.send_to(eb3at_gahez, server_list[i]).expect("gahez bayez");
 
-                socket.send_to(eb3at_gahez, "10.40.55.44:2155").expect("gahez bayez");
-                // let timer = time::Duration::from_secs(5);
-                // thread::sleep(timer);
-                println!("ana ba3at kosom el gahez");
-                let message = socket.recv_from(&mut bufg);
-                match message {
-                    Ok((_,_src_addr)) => {
-                        println!("recieved gahez message");
+                    let message = socket.recv_from(&mut bufg);
+                    match message {
+                        Ok((_,_src_addr)) => {
+                            println!("recieved gahez message");
+                        }
+                        Err(_) =>()
                     }
-                    Err(_) => println!("error when received gahez message")
+                    
+
+                    //let first_digit = bufg[0] - 48;
+                    let first_digit= 1;
+                    let enta_gahez = first_digit;
+                    //let enta_gahez =  i32::from_be_bytes(buf);
+                    gahez_list[i+1] = enta_gahez;
+                    println!("enta_gahez is = {}", enta_gahez);
+
+                    //let enta_gahez: i32 = enta_gahez.into();
                 }
-                let first_digit = 1;        //bufg[1];
-                let enta_gahez = first_digit;
-                //let enta_gahez =  i32::from_be_bytes(buf);
-
-                println!("enta_gahez is = {}", enta_gahez);
-
-                let enta_gahez: i32 = enta_gahez.into();
                 
-                gahez_1 = ana_gahez * enta_gahez;
+                gahez_1 = 1;
+                for i in 0..gahez_list.len()
+                {
+                    gahez_1 = gahez_1 & gahez_list[i];
+                }
+                
+                //gahez_1 = ana_gahez * enta_gahez;
 
                 
             }
-            
             
             {
                         
@@ -198,8 +231,6 @@ fn gahzeen(socket : &UdpSocket, gahez : &Arc<Mutex<i32>>){
                 *gahez_1 = 1;
                         
             }
-            let timer = time::Duration::from_secs(15);
-        
             thread::sleep(timer);
     }
 }
